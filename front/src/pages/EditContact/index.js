6419cdffba911a69f97c238b45c04d8d361df7c0
Rafael Nacle <rafaelnacle@hotmail.com>
2022-07-02 15:54:14 -0300

    Modal, Loader, Error theme input

---
+++ front/src/pages/EditContact/index.js
@@ -0,0 +1,11 @@
+import ContactForm from '../../components/ContactForm';
+import PageHeader from '../../components/PageHeader';
+
+export default function EditContact() {
+  return (
+    <>
+      <PageHeader title="Editar Rafael Nacle" />
+      <ContactForm buttonLabel="Salvar Alterações" />
+    </>
+  );
+}
---
+++ front/src/pages/NewContact/index.js
@@ -0,0 +1,12 @@
+import ContactForm from '../../components/ContactForm';
+import PageHeader from '../../components/PageHeader';
+
+export default function NewContact() {
+  return (
+    <>
+      <PageHeader title="Novo contato" />
+
+      <ContactForm buttonLabel="Cadastrar" />
+    </>
+  );
+}

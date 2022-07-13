6594bbd78b5e303d602d9f30b1e4a53ffb2ce6cb
Rafael Nacle <rafaelnacle@hotmail.com>
2022-07-13 16:56:29 -0300

    Phone regex

--- front/src/components/ContactForm/index.js
+++ front/src/components/ContactForm/index.js
@@ -1,29 +1,93 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
+
+import isEmailValid from '../../utils/isEmailValid';
+import formatPhone from '../../utils/formatPhone';
+import useErrors from '../../hooks/useErrors';
+
+import { ButtonContainer, Form } from './styles';
 
 import FormGroup from '../FormGroup';
-import { ButtonContainer, Form } from './styles';
 import Input from '../Input';
 import Select from '../Select';
 import Button from '../Button';
 
 export default function ContactForm({ buttonLabel }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [category, setCategory] = useState('');
+
+  const { setError, removeError, getErrorMessageByFieldName } = useErrors();
+
+  function handleNameChange(event) {
+    setName(event.target.value);
+
+    if (!event.target.value) {
+      setError({ field: 'name', message: 'Nome é obrigatório.' });
+    } else {
+      removeError('name');
+    }
+  }
+
+  function handleEmailChange(event) {
+    setEmail(event.target.value);
+
+    if (event.target.value && !isEmailValid(event.target.value)) {
+      setError({ field: 'email', message: 'E-mail inválido.' });
+    } else {
+      removeError('email');
+    }
+  }
+
+  function handlePhoneChange(e) {
+    setPhone(formatPhone(e.target.value));
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    console.log({
+      name, email, phone: phone.replace(/\D/g, ''), category,
+    });
+  }
+
   return (
-    <Form>
-      <FormGroup>
-        <Input placeholder="Nome" />
+    <Form onSubmit={handleSubmit} noValidate>
+      <FormGroup error={getErrorMessageByFieldName('name')}>
+        <Input
+          error={getErrorMessageByFieldName('name')}
+          placeholder="Nome"
+          value={name}
+          onChange={handleNameChange}
+        />
       </FormGroup>
-      <FormGroup
-        error="O formato do e-mail é invalido."
-      >
-        <Input placeholder="Email" error />
+      <FormGroup error={getErrorMessageByFieldName('email')}>
+        <Input
+          type="email"
+          error={getErrorMessageByFieldName('email')}
+          placeholder="Email"
+          value={email}
+          onChange={handleEmailChange}
+        />
       </FormGroup>
       <FormGroup>
-        <Input placeholder="Telefone" />
+        <Input
+          placeholder="Telefone"
+          value={phone}
+          onChange={handlePhoneChange}
+          maxLength="15"
+        />
       </FormGroup>
 
       <FormGroup>
-        <Select>
+        <Select
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        >
+          <option value="">Categoria</option>
           <option value="instagram">Instagram</option>
+          <option value="discord">Discord</option>
         </Select>
       </FormGroup>
 

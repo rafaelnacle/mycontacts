a2a427ff45414014a10d674a1b65f90aa2a5caf8
Rafael Nacle <rafaelnacle@hotmail.com>
2022-07-28 14:12:11 -0300

    Switching object to Map

--- front/src/lib/EventManager.js
+++ front/src/lib/EventManager.js
@@ -1,33 +1,56 @@
 export default class EventManager {
   constructor() {
-    this.listeners = {
-
-    };
+    this.listeners = new Map();
   }
 
   on(event, listener) {
-    if (!this.listeners[event]) {
-      this.listeners[event] = [];
+    if (!this.listeners.has(event)) {
+      this.listeners.set(event, []);
     }
 
-    this.listeners[event].push(listener);
+    this.listeners.get(event).push(listener);
   }
 
   emit(event, payload) {
-    if (!this.listeners[event]) {
+    if (!this.listeners.has(event)) {
       return;
     }
 
-    this.listeners[event].forEach((listener) => {
+    this.listeners.get(event).forEach((listener) => {
       listener(payload);
     });
+  }
+
+  removeListener(event, listenerToRemove) {
+    const listeners = this.listeners.get(event);
+
+    if (!listeners) {
+      return;
+    }
+
+    const filteredListeners = listeners.filter(
+      (listener) => listener !== listenerToRemove,
+    );
+
+    this.listeners.set(event, filteredListeners);
   }
 }
 const toastEventManager = new EventManager();
 
-toastEventManager.on('addtoast', (payload) => { console.log('addtoast listener', payload); });
-toastEventManager.on('addtoast', (payload) => { console.log('addtoast listener', payload); });
+function addToast1(payload) {
+  console.log('addtoast listener1', payload);
+}
 
+function addToast2(payload) {
+  console.log('addtoast listener2', payload);
+}
+
+toastEventManager.on('addtoast', addToast1);
+toastEventManager.on('addtoast', addToast2);
 toastEventManager.emit('addtoast', { tupe: 'danger', text: 'Texto' });
+
+toastEventManager.removeListener('addtoast', addToast1);
+
+toastEventManager.emit('addtoast', 'depois de remover');
 
 console.log(toastEventManager);
